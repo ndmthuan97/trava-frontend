6963e94f1a3286bdf022ffbb95cc1b7de7a87b1c
Minh Thuận <ndmthuanwork97@gmail.com>
2026-01-21 20:14:30 +0700

    first commit

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,4 @@
+export const environment = {
+    production: false,
+    baseApiUrl: import.meta.env.BASE_API_URL ?? ''
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,4 @@
+export const environment = {
+    production: true,
+    baseApiUrl: import.meta.env.BASE_API_URL ?? ''
+};
